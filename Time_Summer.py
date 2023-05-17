8a7e77e3c37cddc0945fb093df8c3688ceb220ee
Meet <meetr2512@gmail.com>
2023-05-17 21:46:03 +0530

    Updated the input format

--- Time_Summer.py
+++ Time_Summer.py
@@ -1,14 +1,23 @@
 def Time_Summer():
 
-    print("\nKeep entering your times in (H)*MMSS format. When you want to exit, press 'Enter' without entering anything else. ( (x)* denotes any number of x's. )\n"); 
+    print("\nKeep entering your times in (H)*MMSS format. When you want to exit, press 'Enter' without entering anything. ( (x)* denotes any number of x's. )\n"); 
+    print("Only 'digits' in your input are considered. All other characters are ignored.\n"); 
+
+    nos = ["0","1","2","3","4","5","6","7","8","9"]; 
     hrs=mns=scs = hh=mm=ss = 0 ; 
 
-    x = input("Enter here : "); l = len(x); 
+    def extract_digits(x):
+        y = ""; 
+        for i in x:
+            if(i in nos): y += i; 
+        return y; 
+    
+    x = extract_digits(input("Enter here : ")); l = len(x); 
     while(l!=0):
         if  (l<3): ss += int(x); 
         elif(l<5): mm += int(x[:l-2]); ss += int(x[l-2:]); 
         else:      hh += int(x[:l-4]); mm += int(x[l-4:l-2]); ss += int(x[l-2:]); 
-        x = input("Enter here : "); l = len(x); 
+        x = extract_digits(input("Enter here : ")); l = len(x); 
 
     scs = ss % 60; scs = str(scs); 
     mns = (mm + (ss//60)) % 60; mns = str(mns); 
