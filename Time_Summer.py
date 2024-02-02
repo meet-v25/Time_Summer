0b6b426670097d3d62b72aad6a0df25a757aabb6
Meet <105229321+meet-v25@users.noreply.github.com>
2024-02-02 22:35:36 +0530

    Re-Structure + Add multiplication by *

--- Time_Summer.py
+++ Time_Summer.py
@@ -1,31 +1,60 @@
+digits = ["0","1","2","3","4","5","6","7","8","9"]; 
+
+
+def sum_durations(a,b): # -> "(H)*MMSS" : str
+    a,b = str(a),str(b); l_a,l_b = len(a),len(b); 
+    
+    if  (l_a < 3): secs_a = int(a);         mins_a = 0;                   hors_a = 0; 
+    elif(l_a < 5): secs_a = int(a[l_a-2:]); mins_a = int(a[:l_a-2]);      hors_a = 0; 
+    else:          secs_a = int(a[l_a-2:]); mins_a = int(a[l_a-4:l_a-2]); hors_a = int(a[:l_a-4]); 
+
+    if  (l_b < 3): secs_b = int(b);         mins_b = 0;                   hors_b = 0; 
+    elif(l_b < 5): secs_b = int(b[l_b-2:]); mins_b = int(b[:l_b-2]);      hors_b = 0; 
+    else:          secs_b = int(b[l_b-2:]); mins_b = int(b[l_b-4:l_b-2]); hors_b = int(b[:l_b-4]); 
+
+    secs_tot = secs_a + secs_b; 
+    mins_tot = mins_a + mins_b; 
+    hors_tot = hors_a + hors_b; 
+
+    # res = result
+    secs_res = "00" + str( (secs_tot % 60) ); 
+    mins_res = "00" + str( (mins_tot + (secs_tot//60)) % 60 ); 
+    hors_res = "00" + str( hors_tot + ((mins_tot + (secs_tot//60)) // 60) ); 
+
+    resulting_time = hors_res[-2:] + mins_res[-2:] + secs_res[-2:]; 
+    return resulting_time;  # -> "(H)*MMSS"
+
+
+def parse_input_duration(x):
+    try:            # If input is of the form "time*multiplier" , eg. "130*7" i.e. 7 times of 1 mins 30 secs
+        if(x.count("*")!=1): raise; 
+        pos = x.index("*"); 
+        res = "000000"; tiime = int(x[:pos].strip()); multiplier = int(x[pos+1:].strip()); 
+        for i in range(multiplier): res = sum_durations(res,tiime); 
+        return res; 
+    
+    except:         # Otherwise, remove everything except digits
+        res = ""; 
+        for i in x: res += (i in digits)*i; 
+        return res; 
+
+
 def Time_Summer():
 
     print("\nKeep entering your times in (H)*MMSS format. When you want to exit, press 'Enter' without entering anything. ( (x)* denotes any number of x's. )\n"); 
-    print("Only 'digits' in your input are considered. All other characters are ignored.\n"); 
+    print("Only 'digits' (and single '*' denoting mult) in your input are considered. All other characters are ignored.\n"); 
 
-    nos = ["0","1","2","3","4","5","6","7","8","9"]; 
-    hrs=mns=scs = hh=mm=ss = 0 ; 
-
-    def extract_digits(x):
-        y = ""; 
-        for i in x:
-            if(i in nos): y += i; 
-        return y; 
-    
-    x = extract_digits(input("Enter here : ")); l = len(x); 
+    fin = "000000"; 
+    parsed_input = parse_input_duration(input("Enter here : ")); l = len(parsed_input); 
     while(l!=0):
-        if  (l<3): ss += int(x); 
-        elif(l<5): mm += int(x[:l-2]); ss += int(x[l-2:]); 
-        else:      hh += int(x[:l-4]); mm += int(x[l-4:l-2]); ss += int(x[l-2:]); 
-        x = extract_digits(input("Enter here : ")); l = len(x); 
+        fin = sum_durations(fin,parsed_input); 
+        parsed_input = parse_input_duration(input("Enter here : ")); l = len(parsed_input); 
 
-    scs = ss % 60; scs = str(scs); 
-    mns = (mm + (ss//60)) % 60; mns = str(mns); 
-    hrs = hh + (mm + (ss//60)) // 60; hrs = str(hrs); 
+    l_fin = len(fin); hors_fin = int(fin[:l_fin-4]); mins_fin = int(fin[l_fin-4:l_fin-2]); secs_fin = int(fin[l_fin-2:]); 
+    print(f"\nStandard Total  =  {hors_fin} {mins_fin} {secs_fin}  =  {hors_fin} Hours {mins_fin} Minutes {secs_fin} Seconds . \n"); 
+    input("\nPress Enter to Exit."); print(); 
 
-    if(len(hrs)<2): print("\nStandard Total =", "0"+hrs, "0"*(2-len(mns))+mns, "0"*(2-len(scs))+scs, "=", "0"+hrs, "Hours", "0"*(2-len(mns))+mns, "Minutes", "0"*(2-len(scs))+scs, "Seconds", "\n"); 
-    else:           print("\nStandard Total =",     hrs, "0"*(2-len(mns))+mns, "0"*(2-len(scs))+scs, "=",     hrs, "Hours", "0"*(2-len(mns))+mns, "Minutes", "0"*(2-len(scs))+scs, "Seconds", "\n"); 
 
-    exit = input("\nPress Enter to Exit."); print(); 
+if(__name__ == "__main__"):
+    Time_Summer(); 
 
-Time_Summer(); 
